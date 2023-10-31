e192a67ef11c6df7b090583deec1cb2ca49ea4d7
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-10-31 16:36:28 +0530

    first commit

--- project01/src/main.jsx
+++ project01/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Header/>
+    <Footer/>
   </React.StrictMode>
 );
